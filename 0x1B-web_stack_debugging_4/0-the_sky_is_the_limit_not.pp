95d228497b692e52e4ae02c15d47a26eae5c6e3e
nadiaMahfoud <nadiamahfoud48@gmail.com>
2023-12-20 22:48:47 -0800

    updates

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,17 @@
-# Task: Adjust ULIMIT for Nginx to handle more file descriptors
-# Increase the ULIMIT of the default file
+# Puppet Manifest: Increase Nginx Server Traffic Handling
+
+# Adjust ULIMIT for Nginx to handle more file descriptors
 exec { 'fix--for-nginx':
+## Replace the default ULIMIT value with 4096
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
+# Set the path for the 'sed' command
   path    => '/usr/local/bin/:/bin/'
 }
 
-# Restart Nginx
+# Restart Nginx to apply ULIMIT changes
 -> exec { 'nginx-restart':
+# Restart Nginx to apply the new ULIMIT value
   command => 'nginx restart',
+# Set the path for the 'nginx restart' command
   path    => '/etc/init.d/'
 }
